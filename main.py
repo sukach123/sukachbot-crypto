e9364a839962f4f5674e6cf31d0f4ecb22207eb8
sukach123 <sukachpt@gmail.com>
2025-04-19 21:44:34 +0100

    Update main.py

--- main.py
+++ main.py
@@ -2,9 +2,9 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route("/")
 def home():
-    return "SukachBot CRYPTO está online!"
+    return "SukachBot CRYPTO online e pronto para enviar sinais! 🚀"
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8000)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8000)
