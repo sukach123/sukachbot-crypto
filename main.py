d397ce3dc504282ebe70566084acf687d557b066
sukach123 <sukachpt@gmail.com>
2025-04-21 18:57:16 +0100

    Update main.py

--- main.py
+++ main.py
@@ -114,7 +114,7 @@ def analisar_entradas(par):
         print(f"❌ Par {par} não encontrado na lista de pares válidos.")
         return False
 
-    url = f"https://api.bybit.com/v2/public/kline/list"  # Alterado para o endpoint V2
+    url = f"https://api.bybit.com/v5/market/kline"  # Alterado para o endpoint V5
     params = {
         "symbol": par,
         "interval": "5m",  # Intervalo de 5 minutos
@@ -219,4 +219,5 @@ for par in PARES:
             sl=STOP_LOSS_PORCENTAGEM,
             side="Buy"
         )
+
 
